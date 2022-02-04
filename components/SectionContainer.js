a55a1a2e09a10b567274c74367b914008e8ddc28
kumard3 <kumardeepanshu157200@gmail.com>
2022-02-04 22:42:03 +0530

    changes made

--- components/SectionContainer.js
+++ components/SectionContainer.js
@@ -1,3 +1,5 @@
 export default function SectionContainer({ children }) {
-  return <div className="dark:bg-black w-full mt-2  min-h-screen">  <div className=" px-5 mx-auto container   ">{children}</div></div>
+  return <div className="dark:bg-black w-full mt-2  min-h-screen">  
+ <div className="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>
+  </div>
 }
